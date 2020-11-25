846bc55c29835a48a17eda0594d8810e5fd4a2ee
Paul Chheang <phalcheat@gmail.com>
2020-11-25 12:16:30 -0600

    Created a mongoose model for 'user'

--- models/user.js
+++ models/user.js
@@ -1,13 +1,26 @@
+const mongoose = require("mongoose");
 
-// const User = sequelizeDB.define("user", {
-//     id: {
-//         type: Sequelize.INTEGER,
-//         autoIncrement: true,
-//         allowNull: false,
-//         primaryKey: true
-//     },
-//     name: Sequelize.STRING,
-//     email: Sequelize.STRING
-// });
+// Instantiate a mongoose schema 'userSchema' by calling
+// the constructor of the class 'Schema' of mongoose module.
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    cart: {
+        // Defined embeded document for cart items which should be an array
+        // holding all items that user added to the cart.
+        items: [
+            {
+                // The type of prouctId must be the type of Mongoose's ObjectId
+                productId: { type: mongoose.Schema.Types.ObjectId, required: true },
+                quantity: { type: Number, required: true }
+            }]
+    }
+});
 
-// module.exports = User;
+module.exports = mongoose.model("User", userSchema);
