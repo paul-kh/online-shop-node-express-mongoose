f05b209d3b99912731f5bfc2542a83619b81b768
Paul Chheang <phalcheat@gmail.com>
2020-12-05 14:13:25 -0600

    remove sharp module

--- util/upload-product-image.js
+++ util/upload-product-image.js
@@ -2,7 +2,6 @@ module.exports = uploadProductImage = (app) => {
   // PARSING INCOMING REQUEST'S OF CONTENT TYPE = 'BINARY'
   // Note: The submit form must set enctype = "multipart/form data"
   const multer = require("multer");
-  const sharp = require("sharp");
   const storage = multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, "images");
