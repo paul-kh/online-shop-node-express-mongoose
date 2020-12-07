ae632e0ed6881970efa45c5aa4a90b925021ac6b
Paul Chheang <phalcheat@gmail.com>
2020-12-06 22:16:03 -0600

    added file name for uploaded image

--- util/upload-product-image.js
+++ util/upload-product-image.js
@@ -8,7 +8,7 @@ module.exports = uploadProductImage = (app, fileInput) => {
     },
     filename: (req, file, cb) => {
       const date = new Date();
-      cb(null, file.fieldname + "-" + Date.now());
+      cb(null, Date.now() + "-" + file.originalname);
     },
   });
 
